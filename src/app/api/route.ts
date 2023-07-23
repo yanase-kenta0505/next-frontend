d7954f3361be178572a26b369aacf5762756e87f
yanasekenta <ynsknt0505@gmail.com>
2023-07-23 21:21:38 +0900

    post requestをエラーなく実行できるように修正

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,10 +1,19 @@
+import { NextResponse } from "next/server";
+
 export async function POST(request: Request) {
-  const res = await request.json();
-  console.log('res', res)
-  await fetch('http://localhost:3001/todo', {
-    method: 'POST',
-    body: JSON.stringify(res),
-    headers: {
-      'Content-Type': 'application/json'
-    }})
+  try {
+    const res = await request.json();
+    console.log('res', res)
+    await fetch('http://localhost:3001/todo', {
+      method: 'POST',
+      body: JSON.stringify(res),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+
+    return NextResponse.json({ message: "This Worked", success: true })
+  } catch (error) {
+    return NextResponse.json({ message: error, success: false })
+  }
 }
--- src/app/createTodo/page.tsx
+++ src/app/createTodo/page.tsx
@@ -10,6 +10,8 @@ export default function CreateTodo() {
         userId: 2
       })
     })
+
+    console.log('end')
   }
 
   return (
